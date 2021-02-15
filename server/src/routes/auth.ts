d289f46b461c8d98318e828b10829398749dac86
zsuzsihudacsko <zsuzsi@scitodate.com>
2021-02-15 14:18:36 +0100

    heroku

--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -4,7 +4,7 @@ import {AuthUser} from "../models/auth";
 
 const router = Router();
 
-router.post("/login", (req: Request, res: Response, next: NextFunction) => {
+router.post("/", (req: Request, res: Response, next: NextFunction) => {
     AuthApi.login(req.body.email, req.body.password)
         .then((result: AuthUser) => res.status(200).send(result))
         .catch(next);
--- server/src/routes/pages.ts
+++ server/src/routes/pages.ts
@@ -5,25 +5,25 @@ import {authorize} from "../middlewares/authorize";
 
 const router = Router();
 
-router.get("/pages", (req: Request, res: Response, next: NextFunction) => {
+router.get("/", (req: Request, res: Response, next: NextFunction) => {
     PagesApi.getAllPages()
         .then((result: Array<Page>) => res.status(200).send(result))
         .catch(next);
 });
 
-router.put("/pages", authorize, (req: Request, res: Response, next:NextFunction) => {
+router.put("/", authorize, (req: Request, res: Response, next:NextFunction) => {
     PagesApi.addPage(req.body.type, req.body.title, req.body.subtitle, req.body.images)
         .then((result: Page) => res.status(200).send(result))
         .catch(next);
 })
 
-router.post("/pages/:id", authorize, (req: Request, res: Response, next:NextFunction) => {
+router.post("/:id", authorize, (req: Request, res: Response, next:NextFunction) => {
     PagesApi.editPage(req.body.id, req.body.type, req.body.title, req.body.subtitle, req.body.images)
         .then((result: Page) => res.status(200).send(result))
         .catch(next);
 })
 
-router.post("/pages/:id", authorize, (req: Request, res: Response, next:NextFunction) => {
+router.post("/:id", authorize, (req: Request, res: Response, next:NextFunction) => {
     PagesApi.deletePage(req.body.id)
         .then((result: boolean) => res.status(200).send(result))
         .catch(next);
