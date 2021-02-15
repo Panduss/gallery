2234a4c156356b33cb48278c303d6238fabb669f
zsuzsihudacsko <zsuzsi@scitodate.com>
2021-02-15 15:04:33 +0100

    little cleanup

--- server/build/models/auth.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var AuthUser = /** @class */ (function () {
-    function AuthUser() {
-    }
-    return AuthUser;
-}());
-exports.AuthUser = AuthUser;
-//# sourceMappingURL=auth.js.map
